e4a88d69c37492c6b4933b2bbc3c94f02d6420ba
Rohit Negi <rohitnegi263139@gmail.com>
2025-02-19 16:07:36 +0530

    create signUp Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./App.css";
+import { Route, Routes } from "react-router";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 function App() {
-	return <div>WORLD'S NO. ONE AUCTION APP : AUCTIONVERSE</div>;
+	return (
+		<div className="root">
+			<Routes>
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/signin" element={<SignIn />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
