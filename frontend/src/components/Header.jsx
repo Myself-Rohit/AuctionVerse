a75fbe49ec16095571f40c6e2e8810e517ff3f76
Rohit Negi <rohitnegi263139@gmail.com>
2025-02-20 17:29:50 +0530

    create header ui

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../styles/header.css";
+import { Link } from "react-router";
+function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	return (
+		<nav className="header">
+			<Link className="logo" to="/">
+				AUCTIONVESE
+			</Link>
+			<span className="grow"></span>
+
+			<img
+				id="menu-btn"
+				alt="menu"
+				onClick={() => setIsMenuOpen(!isMenuOpen)}
+				src="https://img.icons8.com/?size=100&id=OTxpMqWbm71F&format=png&color=4bcf39"
+			/>
+
+			<ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+				<Link className="nav-item" to="/dashboard">
+					Dashboard
+				</Link>
+				<Link className="nav-item" to="/postAuction">
+					Post Auction
+				</Link>
+				<Link id="signin-btn" className="nav-item " to="/signin">
+					SignIn
+				</Link>
+				<Link id="signup-btn" className="nav-item " to="/signup">
+					SignUp
+				</Link>
+			</ul>
+		</nav>
+	);
+}
+
+export default Header;
