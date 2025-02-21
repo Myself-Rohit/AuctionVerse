33910d77f9783c88ef16ae17f44995c4705dfd4e
Rohit Negi <rohitnegi263139@gmail.com>
2025-02-21 14:26:41 +0530

    create landing page ui

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "../styles/landing.css";
+import { Link } from "react-router";
 
 function Landing() {
 	return (
 		<div className="landing">
 			<div>
-				<h1>Welcome to AUCTIONVERSE</h1>
+				<h1 className="heading">
+					Welcome to <span>AuctionVerse</span>
+				</h1>
+				<p className="sub-heading"> Where Bidding Meets Opportunity!</p>
+				<Link className="btn" to="/dashboard">
+					Get Started
+				</Link>
 			</div>
 		</div>
 	);
