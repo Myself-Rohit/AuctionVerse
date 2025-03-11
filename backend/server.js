8b2579a0f018d3e5e2cdd65c8014038d81c7584c
Rohit Negi <rohitnegi263139@gmail.com>
2025-03-11 21:35:03 +0530

    update url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://auctionverse-nnw5.onrender.com",
 		methods: ["GET", "POST", "PATCH", "DELETE"],
 		credentials: true,
 	})
